ecac5efeb25e77b2af314a0a20d73a69e0b3bb13
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2024-11-12 10:58:24 -0300

    Feat: Implementing database connection via PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [BookModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT, 10),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    BookModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
